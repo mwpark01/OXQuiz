fdf75589017d72a3d83c65f8f857fc3ecd645576
Minwoo Park <hyminwoo0000@gmail.com>
2025-01-12 13:05:15 +0900

    Initial Commit

---
+++ OXQuiz/OXQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  OXQuizApp.swift
+//  OXQuiz
+//
+//  Created by 박민우 on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OXQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
